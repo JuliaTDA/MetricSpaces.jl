f112556deffbb1d9ec26c85260a5d3802f00685d
Guilherme Vituri <viturivituri@gmail.com>
2026-03-26 02:19:33 -0300

    fix: correct norm and normalize functions for consistency and clarity

--- src/base/norm.jl
+++ src/base/norm.jl
@@ -2,11 +2,11 @@
 """
     Euclidean norm.
 """
-norm(x) = x.^2 |> sum |> sqrt
+norm(x) = sqrt(sum(abs2, x))
 
 normalize(x) = x ./ norm(x)
 
-normalize(X::EuclideanSpace) = map(X, normalize)
+normalize(X::EuclideanSpace) = map(normalize, X)
 
 function normalize!(M::MetricSpace)
     for i ∈ eachindex(M)
