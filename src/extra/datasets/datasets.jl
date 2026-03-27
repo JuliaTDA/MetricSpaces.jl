5d8e2ee1048454fc63557f8bc7f2fb6292481ad3
Guilherme Vituri <viturivituri@gmail.com>
2026-03-27 02:47:31 -0300

    feat: add add_noise function to introduce Gaussian noise to EuclideanSpace

--- src/extra/datasets/datasets.jl
+++ src/extra/datasets/datasets.jl
@@ -1,6 +1,7 @@
 module Datasets
 
 using ..MetricSpaces: EuclideanSpace, normalize, norm
+using StaticArrays: SVector
 
 include("geometric.jl")
 include("topological.jl")
@@ -8,10 +9,23 @@ include("clusters.jl")
 include("stochastic.jl")
 include("mammoth.jl")
 
+"""
+    add_noise(X::EuclideanSpace{N,T}, σ::Number = 0.01) where {N,T}
+
+Add Gaussian noise with standard deviation `σ` to each point in the space.
+Returns a new EuclideanSpace with the noisy points.
+"""
+function add_noise(X::EuclideanSpace{N,T}, σ::Number=0.01) where {N,T}
+    [x + SVector{N,T}(σ .* randn(N)) for x in X]
+end
+
 export sphere, cube, torus, grid, star,
+       swiss_roll, annulus, ellipse, spiral,
        trefoil_knot, linked_rings, unlinked_rings,
+       figure_eight, klein_bottle, mobius_strip, clifford_torus,
        two_clusters, three_clusters, linked_clusters, long_gaussian,
        random_walk, orthogonal_curve,
-       mammoth
+       mammoth,
+       add_noise
 
 end
