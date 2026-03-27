ceb83ef9ae30ee5a41217bd8b6961c5e69ca745c
Guilherme Vituri <viturivituri@gmail.com>
2026-03-27 02:47:15 -0300

    feat: add additional distance functions including cosine, hamming, correlation, and minkowski

--- src/base/distance functions.jl
+++ src/base/distance functions.jl
@@ -4,3 +4,11 @@ dist_euclidean = Distances.euclidean
 dist_cityblock = Distances.cityblock
 
 dist_chebyshev = Distances.chebyshev
+
+dist_cosine = Distances.cosine_dist
+
+dist_hamming = Distances.hamming
+
+dist_correlation = Distances.corr_dist
+
+dist_minkowski(p) = (x, y) -> Distances.minkowski(x, y, p)
